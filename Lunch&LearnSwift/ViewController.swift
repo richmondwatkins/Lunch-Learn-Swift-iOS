bc0e19b054832b8c34ccac97e2d1166c75c16bad
richmondwatkins <richmond@caddis.co>
2015-06-03 08:00:13 -0500

    Initial Commit

---
+++ Lunch&LearnSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lunch&LearnSwift
+//
+//  Created by Richmond Watkins on 6/3/15.
+//  Copyright (c) 2015 Richmond Watkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
